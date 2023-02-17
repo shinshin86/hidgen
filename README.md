1e5e938ce4862fa05f0d74755308ac71c21c8ea4
shinshin86 <beagles1986@gmail.com>
2023-02-17 10:43:13 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# hidgen
+CLI tool for automatic heading tag ID generation.
+
+## Usage
+By default, only h2 and h3 are given the id
+
+`index.html`
+
+```html
+<div>div tag</div>
+<h1>h1 tag</h1>
+<h2>h2 tag</h2>
+<h3>h3 tag</h3>
+<h4>h4 tag</h4>
+<h5>h5 tag</h5>
+<h6>h6 tag</h6>
+```
+
+```sh
+# hidgen <input.html> <output.html>
+hidgen index.html output.html
+```
+
+`output.html`
+
+```html
+<div>div tag</div>
+<h1>h1 tag</h1>
+<a id="h2 tag"></a>
+<h2>h2 tag</h2>
+<a id="h3 tag"></a>
+<h3>h3 tag</h3>
+<h4>h4 tag</h4>
+<h5>h5 tag</h5>
+<h6>h6 tag</h6>
+```
+
+### Assign IDs to all header tags
+
+```sh
+hidgen index.html output.html "h1,h2,h3,h4,h5,h6"
+```
+
+
+```html
+<div>div tag</div>
+<a id="h1 tag"></a>
+<h1>h1 tag</h1>
+<a id="h2 tag"></a>
+<h2>h2 tag</h2>
+<a id="h3 tag"></a>
+<h3>h3 tag</h3>
+<a id="h4 tag"></a>
+<h4>h4 tag</h4>
+<a id="h5 tag"></a>
+<h5>h5 tag</h5>
+<a id="h6 tag"></a>
+<h6>h6 tag</h6>
+```
+
+### Specify any tag to replace
+
+You can also specify any tag to replace, not just headings.
+
+```sh
+hidgen index.html output.html "div"
+```
+
+```html
+<a id="div tag"></a>
+<div>div tag</div>
+<h1>h1 tag</h1>
+<h2>h2 tag</h2>
+<h3>h3 tag</h3>
+<h4>h4 tag</h4>
+<h5>h5 tag</h5>
+<h6>h6 tag</h6>
+```
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const ERR_TXT = "=== hidgen: Error ==="
+
+func usage() {
+	fmt.Println("USAGE: hidgen <input.html> <output.html> <Optional: Heading tag to be replaced>")
+}
+
+func main() {
+	flag.Usage = usage
+
+	if len(os.Args) < 3 {
+		fmt.Println(ERR_TXT)
+		usage()
+		os.Exit(1)
+	}
+
+	inputPath := os.Args[1]
+	outputPath := os.Args[2]
+	replaceHeading := "h2, h3"
+
+	if len(os.Args) == 4 {
+		replaceHeading = os.Args[3]
+	}
+
+	f, err := os.Open(inputPath)
+
+	if err != nil {
+		fmt.Println(ERR_TXT)
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+
+	if err != nil {
+		fmt.Println(ERR_TXT)
+		log.Fatal(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(b)))
+	if err != nil {
+		fmt.Println(ERR_TXT)
+		log.Fatal(err)
+	}
+
+	doc.Find(replaceHeading).Each(func(i int, s *goquery.Selection) {
+		anchor, _ := goquery.NewDocumentFromReader(strings.NewReader("<a></a>"))
+		anchor.Find("a").SetAttr("id", strings.ToLower(s.Text()))
+		html, _ := anchor.Html()
+		s.BeforeHtml(html + "\n")
+	})
+
+	html, err := doc.Find("body").Html()
+	if err != nil {
+		fmt.Println(ERR_TXT)
+		log.Fatal(err)
+	}
+
+	data := []byte(html)
+
+	f2, err := os.Create(outputPath)
+	if err != nil {
+		fmt.Println(ERR_TXT)
+		log.Fatal(err)
+	}
+
+	defer f2.Close()
+
+	_, err = f2.Write(data)
+
+	if err != nil {
+		fmt.Println(ERR_TXT)
+		log.Fatal(err)
+	}
+
+	fmt.Println("=== hidgen: Successful ===")
+}
